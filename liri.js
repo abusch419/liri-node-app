109bd1fa2024a41a5a6bf3f37b2bb6e65e58c7e4
Abusch419 <abusch419@gmail.com>
2019-05-13 18:18:34 -0400

    refactors console.logs and appends with template literals

--- liri.js
+++ liri.js
@@ -11,7 +11,6 @@ let queryUrl;
 let querry;
 let response;
 
-
 inquirer
   .prompt([
     {
@@ -66,24 +65,24 @@ function concertThis() {
     "/events?app_id=db4cdbfd2bad1b7a3e4cdc51a42f15b9";
   axios.get(queryUrl)
     .then(function (response) {
-      console.log(`Showing next event for ${querry}`)
-      console.log(`Venue: ${response.data[0].venue.name}`)
-      console.log(`City: ${response.data[0].venue.city}`)
-      console.log(`State: ${response.data[0].venue.region}`)
-      console.log(`Date: ${moment(response.data[0].datetime).format('YYYY/MM/DD')}`);
-      append(`Showing next event for ${querry}`)
-      append(`Venue: ${response.data[0].venue.name}`)
-      append(`City: ${response.data[0].venue.city}`)
-      append(`State: ${response.data[0].venue.region}`)
-      append(`Date: ${moment(response.data[0].datetime).format('YYYY/MM/DD')}`);
-
-
+      console.log(
+        `Showing next event for ${querry}
+======================================
+Venue: ${response.data[0].venue.name}
+City: ${response.data[0].venue.city}
+State: ${response.data[0].venue.region}
+Date: ${moment(response.data[0].datetime).format('YYYY/MM/DD')}
+=====================================`)
+      append(`Showing next event for ${querry}
+      Venue: ${response.data[0].venue.name}
+      City: ${response.data[0].venue.city}
+      State: ${response.data[0].venue.region}
+      Date: ${moment(response.data[0].datetime).format('YYYY/MM/DD')}`);
     })
     .catch(function (error) {
       console.log(error);
     });
 }
-
 function spotifyThisSong() {
   var Spotify = require('node-spotify-api');
   var spotify = new Spotify(keys.spotify);
@@ -91,61 +90,54 @@ function spotifyThisSong() {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    console.log(`Artist Name: ${data.tracks.items[0].artists[0].name}`);
-    console.log(`Song Name: ${data.tracks.items[0].name}`);
-    console.log(`Preview URL: ${data.tracks.items[0].preview_url}`);
-    console.log(`Album Name: ${data.tracks.items[0].album.name}`);
-    append(`Artist Name: ${data.tracks.items[0].artists[0].name}`);
-    append(`Song Name: ${data.tracks.items[0].name}`);
-    append(`Preview URL: ${data.tracks.items[0].preview_url}`);
-    append(`Album Name: ${data.tracks.items[0].album.name}`);
+    console.log(
+      `====================================================
+Artist Name: ${data.tracks.items[0].artists[0].name}
+Song Name: ${data.tracks.items[0].name}
+Preview URL: ${data.tracks.items[0].preview_url}
+Album Name: ${data.tracks.items[0].album.name}
+=====================================================`)
+    append(`Artist Name: ${data.tracks.items[0].artists[0].name}
+    Song Name: ${data.tracks.items[0].name}
+    Preview URL: ${data.tracks.items[0].preview_url}
+    Album Name: ${data.tracks.items[0].album.name}`)
   });
 }
-
-
-
 function movieThis() {
   queryUrl = "http://www.omdbapi.com/?t=" + querry + "&y=&plot=short&apikey=trilogy";
   axios.get(queryUrl)
     .then(function (response) {
-
-      console.log("Movie Title: " + response.data.Title)
-      console.log("Release Year: " + response.data.Year);
-      console.log("IMDB Rating: " + response.data.imdbRating);
-      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-      console.log("Country Movie Was Produced In: " + response.data.Country);
-      console.log("Language: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Cast: " + response.data.Actors);
-      append("Movie Title: " + response.data.Title)
-      append("Release Year: " + response.data.Year);
-      append("IMDB Rating: " + response.data.imdbRating);
-      append("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-      append("Country Movie Was Produced In: " + response.data.Country);
-      append("Language: " + response.data.Language);
-      append("Plot: " + response.data.Plot);
-      append("Cast: " + response.data.Actors);
-
-
-
+      console.log(
+        `=================================
+Movie Title: ${response.data.Title}
+Release Year: ${response.data.Year}
+IMDB Rating: ${response.data.imdbRating}
+Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+Country Movie Was Produced In: ${response.data.Country}
+Language: ${response.data.Language}
+Plot: ${response.data.Plot}
+Cast: ${response.data.Actors}
+==============================`);
+      append(`Movie Title: ${response.data.Title}
+      Release Year: ${response.data.Year} 
+      IMDB Rating: ${response.data.imdbRating}
+      Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+      Country Movie Was Produced In: ${response.data.Country} 
+      Language: ${response.data.Language}
+      Plot: ${response.data.Plot}
+      Cast: ${response.data.Actors}`);
     })
     .catch(function (error) {
       console.log(error);
     });
 }
-
-
 function append(output) {
   fs.appendFile("./log.txt", output + ", ", function (err) {
-
     // If an error was experienced we will log it.
     if (err) {
       console.log(err);
     }
-
     // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-
-
   });
 }
 
